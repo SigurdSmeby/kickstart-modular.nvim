854f1339fcde713db54ab0414420bab7d594c725
Sigurd Smeby <sigurd.smeby@proton.me>
2025-03-29 15:54:58 +0100

    feat(noice): added dismiss and history commands

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,16 +3,23 @@ return {
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
-    opts = {
-      -- add any options here
-    },
+    opts = {},
     dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
+      -- Include nvim-notify if you want to use the notification view
       'rcarriga/nvim-notify',
     },
+    config = function(_, opts)
+      -- Configure nvim-notify first
+      require('noice').setup(opts)
+      -- Add keybinding to show message history
+      vim.keymap.set('n', '<leader>nh', function()
+        require('noice').cmd 'history'
+      end, { desc = 'Noice history' })
+      -- Add keybinding to dismiss all messages
+      vim.keymap.set('n', '<leader>nd', function()
+        require('noice').cmd 'dismiss'
+      end, { desc = 'Dismiss all notifications' })
+    end,
   },
 }
